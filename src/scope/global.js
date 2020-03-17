aa8a9235b1ca51abfd021465db020013cfba35c0
Juan Carlos Garcia Esquivel <carlos.garcia.esquivel@live.com.mx>
2020-03-16 21:03:33 -0600

    Qué es el Scope y cómo funciona el Global Scope

---
+++ src/scope/global.js
@@ -0,0 +1,10 @@
+var hello = 'Hello';
+let world = 'World';
+const helloWorld = 'Hello World';
+
+const anotherFunction = () => {
+  console.log(hello);
+  console.log(world);
+  console.log(helloWorld);
+};
+anotherFunction();
