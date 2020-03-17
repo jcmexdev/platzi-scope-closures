23553d828f9ffeedbbe5f580ae7897499d00e33f
Juan Carlos Garcia Esquivel <carlos.garcia.esquivel@live.com.mx>
2020-03-17 10:12:57 -0600

    ¿Qué es el hoisting?

---
+++ src/hoisting/index.js
@@ -0,0 +1,13 @@
+a = 2; //
+var a; //
+console.log(a); // 2
+
+console.log(b); // undefined
+var b = 4;
+
+// Ejemplo 3
+nameOfDog("Elmo"); // Elmo
+
+function nameOfDog(name) {
+  console.log(name);
+}
